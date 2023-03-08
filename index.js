237f8f3555a400e7a8e193b113b324d0f0884733
Eduardo-Suarez <draco_008@hotmail.com>
2023-03-07 20:30:17 -0800

    Actualizacion cors 2

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ conectarDB();
 const whitelist = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-  origin: '*'
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true);
+    } else {
+      // No esta permitido
+      callback(new Error("Error de Cors"));
+    }
+  },
 };
 
 app.use(cors(corsOptions));
